66f9f4cd1da20d549b2bec33850fc6b599ee8ead
Ilia FILIN <ilia@filin.eu>
2024-09-02 11:35:24 +0200

    updating state handling based on prev review

--- FanControl/FanControl.cpp
+++ FanControl/FanControl.cpp
@@ -12,14 +12,20 @@ void FanControl::begin() {
 void FanControl::receiveSerialData(byte incomingByte) {
 	checkStateTimeout();
 
-	if (state == WAITING_FOR_DATA) {
-		if (incomingByte == inputMarker) {
-			state = RECEIVING_DATA;
-			lastMiliSec = millis();
-		}
-	} else if (state == RECEIVING_DATA) {
-		outputPinData = incomingByte;
-		writeDataToPin();
-		resetState();
+	switch(state) {
+		case WAITING_FOR_DATA:
+			if (incomingByte == inputMarker) {
+				state = RECEIVING_DATA;
+				lastMiliSec = millis();
+			}
+
+			break;
+
+		case RECEIVING_DATA:
+			outputPinData = incomingByte;
+			writeDataToPin();
+			resetState();
+
+			break;
 	}
 }
