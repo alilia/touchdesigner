0ca7d18533d632f07149c6172fc9a45dd13dff79
Ilia FILIN <ilia@filin.eu>
2024-09-02 11:31:45 +0200

    chatgpt code review - rgbled

--- RGBLed/RGBLed.cpp
+++ RGBLed/RGBLed.cpp
@@ -71,15 +71,21 @@ void RGBLed::update() {
 void RGBLed::receiveSerialData(byte incomingByte) {
 	checkStateTimeout();
 
-	if (state == WAITING_FOR_DATA) {
-		if (incomingByte == inputMarker) {
-			state = RECEIVING_DATA;
-			currentRow = 0;
-			bytesReceived = 0;
-			lastMiliSec = millis();
-		}
-	} else if (state == RECEIVING_DATA) {
-		processIncomingByte(incomingByte);
+	switch(state) {
+		case WAITING_FOR_DATA:
+			if (incomingByte == inputMarker) {
+				state = RECEIVING_DATA;
+				currentRow = 0;
+				bytesReceived = 0;
+				lastMiliSec = millis();
+			}
+
+			break;
+
+		case RECEIVING_DATA:
+			processIncomingByte(incomingByte);
+
+			break;
 	}
 }
 
